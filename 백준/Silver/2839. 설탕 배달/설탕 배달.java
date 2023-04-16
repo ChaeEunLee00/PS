9b448fc1422a90bcb2f4823bf98de11780cec3c4
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-04-16 14:34:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -9,15 +9,16 @@ public class Main{
 
         int count = 0;
 
-        int five = N / 5;
-        for(int i = five; i >= 0; i--){
+        int fiveCount = N / 5;
+        for(int i = fiveCount; i >= 0; i--){
 
             int remainder = N - i * 5;
             if(remainder % 3 == 0){
                 count = i + remainder / 3;
                 break;
             }
-            else count = -1;
+            
+            if(i == 0) count = -1;
         }
 
         System.out.println(count);
