0e8560bf4582665fd7756201a277b15c320ce4cf
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-04-16 13:32:42 +0900

    [Bronze II] Title: 대표값2, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] score = new int[5];
+
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            score[i] = sc.nextInt();
+            sum += score[i];
+        }
+        System.out.println(sum/5);
+
+        Arrays.sort(score);
+        System.out.println(score[2]);
+    }
+}
