e42e570b607acee2a20ba0c504cc0d16494e6b59
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-04-16 18:05:46 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2388 ms, Memory: 185276 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int[][] coordinates = new int [num][2];
+        for(int i = 0; i < num; i++){
+            coordinates[i][0] = sc.nextInt();
+            coordinates[i][1] = sc.nextInt();
+        }
+        Arrays.sort(coordinates, (o1, o2) -> {
+            if(o1[0] == o2[0]) return o1[1] - o2[1];
+            else return o1[0] - o2[0];
+        });
+
+        for(int i = 0; i < num; i++){
+            System.out.println(coordinates[i][0] + " "+ coordinates[i][1]);
+        }
+    }
+}
