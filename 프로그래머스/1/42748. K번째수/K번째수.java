42f1da065f10da2d45e1a2565178266201c2c5f0
ChaeEunLee00 <76944115+ChaeEunLee00@users.noreply.github.com>
2024-08-23 09:48:48 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++){
+            int[] command = commands[i];
+            int[] arrCopy = Arrays.copyOfRange(array,command[0]-1, command[1]);
+            Arrays.sort(arrCopy);
+            answer[i] = arrCopy[command[2]-1];    
+        }     
+        
+        return answer;
+    }
+}
