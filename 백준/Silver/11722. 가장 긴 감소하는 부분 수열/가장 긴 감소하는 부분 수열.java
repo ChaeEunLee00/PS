54274369a1534fc914e4a89a98cdaee65812dc84
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-01-14 13:39:23 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 124 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,32 +1,27 @@
+import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+         
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
-        
         for(int i = 0; i < N; i++){
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
         
         int[] dp = new int[N];
-        for(int i = 0; i < N; i++){
-            dp[i] = 1;
-        }
-        
-        //A[i] 보다 앞에 있고, 더 큰 수의 dp 값 중 최댓값
-        for(int i = 0; i < N; i++){
+        for(int i = 1; i < N; i++){
             for(int j = 0; j < i; j++){
-                if(A[j] > A[i]) dp[i] = Math.max(dp[i], dp[j]+1);
+                if(A[j] > A[i]){
+                    if(dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+                }
             }
         }
-        
         Arrays.sort(dp);
-        
-        System.out.println(dp[N-1]);
-        sc.close();
-    
+        System.out.println(dp[N-1]+1);
+        br.close();
     }
 }
