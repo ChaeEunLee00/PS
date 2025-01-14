3d68049c7f2abfbfe78caf5ff9d14ee827cdc690
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-01-14 11:39:45 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,43 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-        
-        for(int i = 0; i < N; i++){
-            arr[i] = sc.nextInt();
+        int[] wine = new int[N+1];
+        for(int i = 1; i < N+1; i++){
+            wine[i] = Integer.parseInt(br.readLine());
         }
         
-        int answer = 0;
-        // 1~3잔 예외처리
-        if (N == 1) {
-            answer = arr[0];
-        } else if (N == 2) {
-            answer = arr[0] + arr[1];
-        } else if (N == 3) {
-            answer = Math.max(arr[0] + arr[1], Math.max(arr[1] + arr[2], arr[0] + arr[2]));
-        } else {
-            // 4잔~
-            
-            // 1~3잔 dp값 설정
-            dp[0] = arr[0];
-            dp[1] = arr[0] + arr[1];
-            dp[2] = Math.max(arr[0] + arr[1], Math.max(arr[1] + arr[2], arr[0] + arr[2]));
-            
-            // dp[i] = (arr[i]+arr[i-1]+dp[i-3], arr[i]+ dp[i-2], dp[i-1]) 중에 최댓값
-            for (int i = 3; i < N; i++) {
-                dp[i] = Math.max(dp[i - 1], Math.max(arr[i] + dp[i - 2], arr[i] + arr[i - 1] + dp[i - 3]));
+        if(N == 1) System.out.println(wine[1]);
+        else if(N == 2) System.out.println(wine[1] + wine[2]);
+        else{
+            int[] dp = new int[N+1];
+            dp[1] = wine[1];
+            dp[2] = wine[1] + wine[2];
+            for(int i = 3; i < N+1; i++){
+                dp[i] = Math.max(wine[i]+wine[i-1]+dp[i-3], Math.max(wine[i]+dp[i-2], dp[i-1]));
             }
-            
-            answer = dp[N-1];
+            System.out.println(dp[N]);
         }
-        
-        // 출력
-        System.out.println(answer);
-
+        br.close();
     }
 }
