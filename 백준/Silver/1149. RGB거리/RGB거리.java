07aa3eb6887e42b2e31f0e6b5301b2223790fd2b
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-01-14 16:49:30 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,35 +1,28 @@
+import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         
-        int N = sc.nextInt();
-        
-       // red = 0, green = 1, blue = 2
-        int[][] cost = new int[N][3];
-       
-        for(int i = 0; i < N; i++){
-            cost[i][0] = sc.nextInt();
-            cost[i][1] = sc.nextInt();
-            cost[i][2] = sc.nextInt();
+        StringTokenizer st;
+        int[][] RGB = new int [N+1][3];
+        for(int i = 1; i < N+1; i++){
+            st = new StringTokenizer(br.readLine());
+            RGB[i][0] = Integer.parseInt(st.nextToken());
+            RGB[i][1] = Integer.parseInt(st.nextToken());
+            RGB[i][2] = Integer.parseInt(st.nextToken());
         }
         
-        // 행은 N, 열은 RGB
-        // dp[i][j]= i번째집을 j색으로 칠할 때 최솟값
-        int[][] dp = new int[N][3];
-        dp[0][0] = cost[0][0];
-        dp[0][1] = cost[0][1];
-        dp[0][2] = cost[0][2];
-        
-        // dp[i][j] = min(dp[i-1][a],dp[i-1][b]) + cost[i][j] => a,b는 j를 제외한 색
-        for(int i = 1; i < N; i++){
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2]; 
+        int[][] dp = new int [N+1][3];
+        for(int i = 1; i < N+1; i++){
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + RGB[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + RGB[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + RGB[i][2];
         }
         
-        System.out.println(Math.min(Math.min(dp[N-1][0],dp[N-1][1]), dp[N-1][2]));
-        sc.close();   
+        System.out.println(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
+        br.close();
     }
 }
