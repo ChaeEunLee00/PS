d04391837b9aeb15ea2e05d5a971e92af391811d
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-02-11 13:57:14 +0900

    [Silver II] Title: 나무 자르기, Time: 492 ms, Memory: 119548 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -10,23 +10,24 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        int maxH = 0;
         int[] trees = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-           trees[i] = Integer.parseInt(st.nextToken());
+            trees[i] = Integer.parseInt(st.nextToken());
+            if(trees[i] > maxH) maxH = trees[i];
         }
 
-        Arrays.sort(trees);
-
         int start = 0;
-        int end = trees[N-1];
+        int end = maxH;
         while(start < end - 1){
             int mid = (start+end) / 2;
 
             // 자른 나무 길이 구하기
             long totalLength = 0;
-            for(int i = N-1; i >= 0; i--){
-                if(trees[i] <= mid) break;
+            for(int i = 0; i < N; i++){
+                if(trees[i] <= mid) continue;
                 totalLength += trees[i]-mid;
             }
 
