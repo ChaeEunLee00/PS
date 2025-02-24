a75c660494711255ed5fc502567c0787b892b8b1
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-02-24 14:54:24 +0900

    [Gold V] Title: 링크와 스타트, Time: 824 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -2,48 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, result;		
-    static int[][] player;		
-    static boolean[] visit;		
-    public static void main(String[] args) throws Exception {
+    static int N;
+    static int[][] ability;
+
+    static boolean[] isSelected;
+    static int min =  Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        player = new int[N][N];
 
-        for(int i = 0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<N; j++){
-                player[i][j] = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+        ability = new int[N][N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                ability[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-		
-        result = Integer.MAX_VALUE;
-        visit = new boolean[N];
-        solve(0);
-        System.out.println(result);
+
+        isSelected = new boolean[N];
+        visit(0);
+        System.out.println(min);
     }
-	
-    static void solve(int cnt){
-        if(cnt == N){
+
+    public static void visit(int idx){
+        if(idx == N){
             int start = 0;
             int link = 0;
-            for(int i = 0; i<N; i++){
-                for(int j = i+1; j<N; j++){
-                    if(visit[i] != visit[j]) continue;
-                    if (visit[i])
-                        start += player[i][j] + player[j][i];
-                    else
-                        link += player[i][j] + player[j][i];
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(isSelected[i] == isSelected[j]){
+                        if(isSelected[i]) start += ability[i][j];
+                        else link += ability[i][j];
+                    }
                 }
             }
-            int diff = Math.abs(start - link);
-            if(diff < result) result = diff;
 
+            min = Math.min(min, Math.abs(start-link));
             return;
         }
-        visit[cnt] = true;
-        solve(cnt+1);
-        visit[cnt] = false;
-        solve(cnt+1);
+        // 선택 ㅇ
+        isSelected[idx] = true;
+        visit(idx+1);
+        // 선택 x
+        isSelected[idx] = false;
+        visit(idx+1);
     }
 }
