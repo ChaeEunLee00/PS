7a2883845fdb72d94807c1e51f48aca653bd7e32
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-02-25 16:00:18 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 440 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -1,104 +1,97 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    // 좌 -> 하 -> 우 -> 상
+    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[][] sdy = {
+            {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+            {-1, -1, 0, 0, 0, 0, 1, 1, 2, 1},
+            {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+            {1, 1, 0, 0, 0, 0, -1, -1, -2, -1}
+    };
+    static int[][] sdx = {
+            {1, 1, 0, 0, 0, 0, -1, -1, -2, -1},
+            {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+            {-1, -1, 0, 0, 0, 0, 1, 1, 2, 1},
+            {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0}
+    };
+    static int[] per = {1, 1, 2, 2, 7, 7, 10, 10, 5, 0};
+
     static int N;
-    static int[][] map;
-    static int[] dx = {0,1,0,-1};   //토네이토의 x 이동 방향
-    static int[] dy = {-1,0,1,0};   //토네이토의 y 이동 방향
-    static int[] dc = {1,1,2,2};   // 토네이도의 각 방향으로 이동하는 횟수
-    static int[][] dsx = {{-1,1,-2,-1,1,2,-1,1,0}, {-1,-1,0,0,0,0,1,1,2},    //모래가 퍼지는 x방향
-                          {1,-1,2,1,-1,-2,1,-1,0}, {1,1,0,0,0,0,-1,-1,-2}};
-    static int[][] dsy = {{1,1,0,0,0,0,-1,-1,-2},{-1,1,-2,-1,1,2,-1,1,0},    //모래가 퍼지는 y방향
-                          {-1,-1,0,0,0,0,1,1,2},{1,-1,2,1,-1,-2,1,-1,0}};
-    static int[] sandRatio ={1,1,2,7,7,2,10,10,5};
-    public static void main(String[] args) throws Exception{
+    static int[][] A;
+    static int sandOver;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine().trim());
-        map = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        A = new int[N][N];
 
-        for(int r=0; r<N; r++){
-            st = new StringTokenizer(br.readLine()," ");
-            for(int c=0; c<N; c++){
-                map[r][c] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int result = calculateOutSand(N/2, N/2);
-        bw.write(String.valueOf(result));
-        bw.flush();
-
-
-
-
+        move(N/2, N/2);
+        System.out.println(sandOver);
     }
-    //현재위치에서 이동 -> 이동한 위치의 모래 뿌리기 -> 이동한위치를 현재위치로 업데이트
-    static int calculateOutSand(int x, int y){
-        int totalOutSand = 0;
 
-        int currentX = x;
-        int currentY = y;
+    public static void move(int y, int x){
+        int[] moveCnt = {1, 1, 2, 2};
 
-        while (true) {
-            for(int d = 0; d<4; d++){
-                for(int moveCount = 0; moveCount<dc[d]; moveCount++){
-                    //현재위치에서 이동
-                    int nextX = currentX+dx[d];
-                    int nextY = currentY+dy[d];
+        while(true){
+            for(int d = 0; d < 4; d++){
+                for(int i = 0; i < moveCnt[d]; i++){
+                    y += dy[d];
+                    x += dx[d];
+                    spread(y, x, d);
 
-                    if(nextX<0 || nextY<0 || nextX>=N ||nextY>=N){
-                        return totalOutSand;
-                    }
-
-                    //이동한 위치의 모래 뿌리기
-                    int sand = map[nextX][nextY];
-                    map[nextX][nextY] = 0;
-                    int spreadTotal = 0;
-
-
-                    for(int spread = 0; spread<9; spread++){
-                        int sandX = nextX + dsx[d][spread];
-                        int sandY = nextY + dsy[d][spread];
-                        int spreadAmount = (sand*sandRatio[spread])/100;
-
-                        if(sandX<0 || sandX>=N || sandY<0 || sandY>=N){
-                            totalOutSand += spreadAmount;
-                        }
-                        else{
-                            map[sandX][sandY]+=spreadAmount;
-                        }
-                        spreadTotal+= spreadAmount;
-                    }
-
-                    //알파
-                    int alphaX = nextX+dx[d];
-                    int alphaY = nextY+dy[d];
-                    int alphaAmount = sand -spreadTotal;
-                    if(alphaX<0 || alphaX>=N || alphaY<0|| alphaY>=N){
-                        totalOutSand +=alphaAmount;
-                    }
-                    else{
-                        map[alphaX][alphaY] +=alphaAmount;
-                    }
-
-
-                    //이동한 위치를 현재위치로 업데이트
-                    currentX = nextX;
-                    currentY = nextY;
+                    if(y == 0 && x == 0) return;
                 }
             }
 
-            //횟수 업데이트
-            for(int index = 0; index<4; index++){
-                dc[index] +=2;
+            for(int d = 0; d < 4; d++){
+                moveCnt[d] += 2;
             }
         }
     }
 
+    public static void spread(int y, int x, int d){
+        int sand = A[y][x];
+        A[y][x] = 0;
+
+        int perY = 0;
+        int perX = 0;
+        int movedSand = 0;
+        for(int i = 0; i < 10; i++){
+            perY = y + sdy[d][i];
+            perX = x + sdx[d][i];
+
+            if(i == 9){
+                if(!isValid(perY,perX)){
+                    sandOver += sand - movedSand;
+                } else{
+                    A[perY][perX] += sand - movedSand;
+                }
+            } else{
+                int perSand = sand * per[i] / 100;
+                if(!isValid(perY,perX)){
+                    sandOver += perSand;
+                } else{
+                    A[perY][perX] += perSand;
+                }
+                movedSand += perSand;
+            }
+        }
+    }
+
+    public static boolean isValid(int y, int x){
+        if(y >= 0 && y < N && x >= 0 && x < N) return true;
+        else return false;
+    }
 }
