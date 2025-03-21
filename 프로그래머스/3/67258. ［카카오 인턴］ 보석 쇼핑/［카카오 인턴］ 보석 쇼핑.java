8486e02af935b553c0c265605af1958c2cde783f
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-03-21 21:23:40 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 79.13 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,6 +1,41 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        
+        Set<String> typeSet = new HashSet(Arrays.asList(gems));
+        int type = typeSet.size();
+        
+        Set<String> set = new HashSet<>();
+        Map<String,Integer> hm = new HashMap<>();
+        
+        int start = 0, end = 0;
+        int min = Integer.MAX_VALUE;
+        while(end < gems.length){
+            hm.put(gems[end], hm.getOrDefault(gems[end],0)+1);
+            set.add(gems[end]);
+            
+            if(set.size() == type){
+                while(hm.get(gems[start])-1 > 0){
+                    hm.put(gems[start],hm.get(gems[start])-1);
+                    start++;
+                }
+                
+                if(end-start < min){
+                    min = end-start;
+                    answer[0] = start+1;
+                    answer[1] = end+1;
+                }
+                
+                hm.put(gems[start],hm.get(gems[start])-1);
+                set.remove(gems[start]);
+                start++;
+            }
+            end++;
+        }
+        
+        
         return answer;
     }
 }
