e9faeff024d255ba476ba2411bac637899060b64
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-03-25 19:25:34 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 13.49 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,30 +1,61 @@
 import java.util.*;
 
 class Solution {
+    public class Job{
+        int num, start, duration;
+        public Job(int num, int start, int duration){
+            this.num = num;
+            this.start = start;
+            this.duration = duration;
+        }
+    }
+    
     public int solution(int[][] jobs) {
-        // 작업이 끝나는 시간 기준으로 오름차순 힙 이용
-        PriorityQueue<Array<int>> heap = new PriorityQueue<Array<int>>(new Comparator<Integer>() {
+        int N = jobs.length;
+        
+        // job 정렬 - 시작 시간 순
+        ArrayList<Job> jobList = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            jobList.add(new Job(i,jobs[i][0],jobs[i][1]));
+        }
+        Collections.sort(jobList, (o1, o2) -> o1.start-o2.start);
+        
+        //우선순위: 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것 순
+        PriorityQueue<Job> pq = new PriorityQueue<Job>(new Comparator<Job>(){
             @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1],o2[1]);
+            public int compare(Job o1, Job o2){
+                if(o1.duration == o2.duration){
+                    if(o1.start == o2.start) return o1.num - o2.num;
+                    else return o1.start - o2.start;
+                }
+                else return o1.duration - o2.duration;
             }
         });
         
-        for(int i = 0; i < jobs.length; i++){
-            heap.add(jobs[i]);
-        }
-        
-        int answer = 0;
-        for(int i = 0; i < jobs.length; i++){
-            int[] currentJob = heap.poll();
-            if(answer < currentJob[0]){
-                answer = currentJob[0]; 
+        int curJob = 0;
+        int curTime = 0;
+        int execute = 0;
+        int turnaround = 0;
+        while(execute < N){
+            // 요청들어온 작업 대기큐에 넣기
+            while(curJob < N && jobList.get(curJob).start <= curTime){
+                pq.offer(jobList.get(curJob));
+                curJob++;
             }
-            answer += currentJob[1] - currentJob[0];
+        
+            // 우선순위 높은 작업 수행
+            if(pq.isEmpty()){
+                curTime++;
+            }
+            else{
+                Job job = pq.poll();
+                curTime += job.duration;
+                turnaround += (curTime-job.start);
+                execute++;
+                System.out.println(job.num);
+            }
         }
         
-        
-        
-        return answer/3;
+        return turnaround / N;
     }
 }
