38a48c2e15b85c4cd52e4e4a450b805e742c73f7
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-07-28 22:13:22 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 340 ms, Memory: 38812 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -9,80 +9,39 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        boolean[][] train = new boolean[N][20];
+        int[] train = new int[N];
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int statementNum = Integer.parseInt(st.nextToken());
+            int trainNum = Integer.parseInt(st.nextToken())-1;
 
-            int trainNum = 0;
             int seatNum = 0;
             if(statementNum == 1){
-                trainNum = Integer.parseInt(st.nextToken())-1;
                 seatNum = Integer.parseInt(st.nextToken())-1;
-                doStatementOne(train, trainNum, seatNum);
+                train[trainNum] |= (1 << seatNum);
+
             }
             else if(statementNum == 2){
-                trainNum = Integer.parseInt(st.nextToken())-1;
                 seatNum = Integer.parseInt(st.nextToken())-1;
-                doStatementTwo(train, trainNum, seatNum);
+                train[trainNum] &= ~(1 << seatNum);
             }
             else if(statementNum == 3){
-                trainNum = Integer.parseInt(st.nextToken())-1;
-                doStatementThree(train, trainNum);
+                train[trainNum] <<= 1;
+                train[trainNum] &= ~(1 << 20);
             }
             else if(statementNum == 4){
-                trainNum = Integer.parseInt(st.nextToken())-1;
-                doStatementFour(train, trainNum);
+                train[trainNum] >>= 1;
             }
         }
 
         System.out.println(countPassedTrain(train));
     }
 
-    public static void doStatementOne(boolean[][] train, int trainNum, int seatNum){
-        if(!train[trainNum][seatNum]) train[trainNum][seatNum] = true;
-    }
-
-    public static void doStatementTwo(boolean[][] train, int trainNum, int seatNum){
-        if(train[trainNum][seatNum]) train[trainNum][seatNum] = false;
-    }
-
-    public static void doStatementThree(boolean[][] train, int trainNum){
-        boolean temp = false;
-        boolean prev = false;
-        for(int i = 0; i < 20; i++){
-            temp = train[trainNum][i];
-            train[trainNum][i] = prev;
-            prev = temp;
-        }
-    }
-
-    public static void doStatementFour(boolean[][] train, int trainNum){
-        boolean temp = false;
-        boolean prev = false;
-        for(int i = 19; i >= 0; i--){
-            temp = train[trainNum][i];
-            train[trainNum][i] = prev;
-            prev = temp;
-        }
-    }
-
-    public static int countPassedTrain(boolean[][] train){
-        String trainStaus = "";
-        Set<String> trainStausSet = new HashSet<>();
-
+    public static int countPassedTrain(int[] train){
+        Set<Integer> set = new HashSet<>();
         for(int i = 0; i < train.length; i++){
-            trainStaus = "";
-            for(int j = 0; j < 20; j++){
-                if(train[i][j]) trainStaus += "1";
-                else trainStaus += "0";
-            }
-
-            if(!trainStausSet.contains(trainStaus)){
-                trainStausSet.add(trainStaus);
-            }
+            if(!set.contains(train[i])) set.add(train[i]);
         }
-
-        return trainStausSet.size();
+        return set.size();
     }
 }
