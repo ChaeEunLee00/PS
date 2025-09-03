37613bff1670526d3744fab26335737b80ec25fe
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-09-03 21:57:50 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 31.41 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int n;
+    public int m;
+    public int r;
+    public int c;
+    public int y;
+    public int x;
+    public int k;
+    public String answer;
+    
+    public int[] dx = {1, 0, 0, -1};
+    public int[] dy = {0, -1, 1, 0};
+    public char[] dc = {'d', 'l', 'r', 'u'};
+    
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        this.n = n;
+        this.m = m;
+        this.r = r;
+        this.c = c;
+        this.y = y;
+        this.x = x;
+        this.k = k;
+        this.answer = "";
+        
+        int dist = Math.abs(x-r) + Math.abs(y-c);
+        if(k < dist) return "impossible";
+        if(dist % 2 == 0 && k % 2 != 0) return "impossible";
+        if(dist % 2 != 0 && k % 2 == 0) return "impossible";
+        
+        dfs(x, y, "");
+
+        return answer;
+    }
+    
+    public void dfs(int curX, int curY, String route){
+        if(!answer.equals("")) return;
+        
+        if(route.length() == k){
+            if(curX == r && curY == c) answer = route;
+            return;
+        }
+        
+        if(route.length() + Math.abs(curX-r) + Math.abs(curY-c) > k) return;
+        
+        for(int i = 0; i < 4; i++){
+            int nextX = curX + dx[i];
+            int nextY = curY + dy[i];
+            
+            if(nextX <= n && nextX > 0 && nextY <= m && nextY > 0){
+                dfs(nextX, nextY, route + dc[i]);
+            }
+        }
+    }
+}
