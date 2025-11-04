3455119c6b38de2bbc17a182b15f75bbef359a11
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2025-11-04 17:04:46 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 작성해주세요
+SELECT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d
+WHERE 
+    d.SKILL_CODE 
+    & (SELECT SUM(s.CODE)
+       FROM SKILLCODES s
+       WHERE s.NAME = 'Python' or s.NAME = 'C#') 
+    != 0
+ORDER BY d.ID;
